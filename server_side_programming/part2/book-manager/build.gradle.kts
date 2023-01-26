4f9e257f6fb5dbb2928c87a90928ec68407aedcd
tomonakar <tomohisa.nak@gmail.com>
2023-01-26 18:00:45 +0900

    初期環境構築:mbGeneratorの実行まで

---
+++ server_side_programming/part2/book-manager/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.8"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("com.arenagod.gradle.MybatisGenerator") version "1.4" // 追加
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+
+}
+
+group = "com.book.manager"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.3.0")
+	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1")
+	implementation("mysql:mysql-connector-java:8.0.23")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.session:spring-session-data-redis")
+	implementation("redis.clients:jedis:3.5.1")
+	implementation("org.springframework.boot:spring-boot-starter-aop")
+	mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+	testImplementation("org.assertj:assertj-core:3.19.0")
+	testImplementation("org.mockito:mockito-core:3.8.0")
+	testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+mybatisGenerator {
+	verbose = true
+	configFile = "${projectDir}/src/main/resources/generatorConfig.xml"
+}
---
+++ server_side_programming/part2/book-manager/src/main/kotlin/com/book/manager/domain/enam/RoleType.kt
@@ -0,0 +1,3 @@
+package com.book.manager.domain.enam
+
+enum class RoleType { ADMIN, USER}
